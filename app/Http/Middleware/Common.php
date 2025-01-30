53f061ec21e32ed30a84308745fb4dbe15dd119a
shazeedul <syedshazeedul@gmail.com>
2025-01-30 15:20:32 +0600

    all worked for RFQ create initial

--- app/Http/Middleware/Common.php
+++ app/Http/Middleware/Common.php
@@ -4,11 +4,12 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
-use DB;
-use Auth;
-use Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
 
 class Common
 {
@@ -18,33 +19,31 @@ class Common
             URL::forceScheme('https');
         }*/
         //get general setting value
-        $general_setting =  Cache::remember('general_setting', 60*60*24*365, function () {
+        $general_setting =  Cache::remember('general_setting', 60 * 60 * 24 * 365, function () {
             return DB::table('general_settings')->latest()->first();
         });
 
         $todayDate = date("Y-m-d");
-        if($general_setting->expiry_date) {
+        if ($general_setting->expiry_date) {
             $expiry_date = date("Y-m-d", strtotime($general_setting->expiry_date));
-            if($todayDate > $expiry_date) {
+            if ($todayDate > $expiry_date) {
                 auth()->logout();
                 return redirect()->route('contactForRenewal');
             }
         }
         //setting language
-        if(isset($_COOKIE['language'])) {
-            \App::setLocale($_COOKIE['language']);
-        }
-        else {
-            \App::setLocale('en');
+        if (isset($_COOKIE['language'])) {
+            App::setLocale($_COOKIE['language']);
+        } else {
+            App::setLocale('en');
         }
         //setting theme
-        if(isset($_COOKIE['theme'])) {
+        if (isset($_COOKIE['theme'])) {
             View::share('theme', $_COOKIE['theme']);
-        }
-        else {
+        } else {
             View::share('theme', 'light');
         }
-        $currency = Cache::remember('currency', 60*60*24*365, function () {
+        $currency = Cache::remember('currency', 60 * 60 * 24 * 365, function () {
             $settingData = DB::table('general_settings')->select('currency')->latest()->first();
             return \App\Models\Currency::find($settingData->currency);
         });
@@ -55,30 +54,30 @@ class Common
 
         $alert_product = DB::table('products')->where('is_active', true)->whereColumn('alert_quantity', '>', 'qty')->count();
         $dso_alert_product = DB::table('dso_alerts')->select('number_of_products')->whereDate('created_at', date("Y-m-d"))->first();
-        if($dso_alert_product)
+        if ($dso_alert_product)
             $dso_alert_product_no = $dso_alert_product->number_of_products;
         else
             $dso_alert_product_no = 0;
         View::share(['alert_product' => $alert_product, 'dso_alert_product_no' => $dso_alert_product_no]);
-        $role = Cache::remember('user_role', 60*60*24*365, function () {
+        $role = Cache::remember('user_role', 60 * 60 * 24 * 365, function () {
             return DB::table('roles')->find(Auth::user()->role_id);
         });
         View::share('role', $role);
-        $permission_list = Cache::remember('permissions', 60*60*24*365, function () {
+        $permission_list = Cache::remember('permissions', 60 * 60 * 24 * 365, function () {
             return DB::table('permissions')->get();
         });
         View::share('permission_list', $permission_list);
-        $role_has_permissions = Cache::remember('role_has_permissions', 60*60*24*365, function () {
+        $role_has_permissions = Cache::remember('role_has_permissions', 60 * 60 * 24 * 365, function () {
             return DB::table('role_has_permissions')->where('role_id', Auth::user()->role_id)->get();
         });
         View::share('role_has_permissions', $role_has_permissions);
 
-        $role_has_permissions_list = Cache::remember('role_has_permissions_list'.Auth::user()->role_id, 60*60*24*365, function () {
+        $role_has_permissions_list = Cache::remember('role_has_permissions_list' . Auth::user()->role_id, 60 * 60 * 24 * 365, function () {
             return DB::table('permissions')->join('role_has_permissions', 'permissions.id', '=', 'role_has_permissions.permission_id')->where('role_id', Auth::user()->role_id)->select('permissions.name')->get();
         });
         View::share('role_has_permissions_list', $role_has_permissions_list);
 
-        $categories_list = Cache::remember('category_list', 60*60*24*365, function () {
+        $categories_list = Cache::remember('category_list', 60 * 60 * 24 * 365, function () {
             return DB::table('categories')->where('is_active', true)->get();
         });
         View::share('categories_list', $categories_list);
--- config/app.php
+++ config/app.php
@@ -176,8 +176,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
-
-
+        Spatie\Permission\PermissionServiceProvider::class,
     ],
 
     /*
