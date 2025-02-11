a280ca7c24d2b543d2df14a5ff85bcd83d4172f7
Saidul Islam <saidulislam197120@gmail.com>
2025-02-11 21:59:52 +0600

    The field change field issue on payments table has resolved

--- database/migrations/2018_05_13_082847_add_payment_id_and_change_sale_id_to_payments_table.php
+++ database/migrations/2018_05_13_082847_add_payment_id_and_change_sale_id_to_payments_table.php
@@ -16,7 +16,7 @@ class AddPaymentIdAndChangeSaleIdToPaymentsTable extends Migration
         Schema::table('payments', function (Blueprint $table) {
             $table->integer('purchase_id')->nullable()->after('id');
             $table->integer('sale_id')->nullable()->change();
-            $table->decimal('change', 10, 2)->nullable();
+            // $table->decimal('change', 10, 2)->nullable();
         });
     }
 
