02855dc0db9f581f56b1ef32743b91e8325eb044
shazeedul <syedshazeedul@gmail.com>
2025-02-13 18:36:59 +0600

    refactor

--- resources/views/backend/rf_quotation/supplier-wise.blade.php
+++ resources/views/backend/rf_quotation/supplier-wise.blade.php
@@ -7,10 +7,34 @@
                     <h3 class="text-center">{{ trans('file.Supplier Wise RFQ Show') }}</h3>
                 </div>
                 <div class="card-body">
-                    
+                    <p class="italic">
+                        <small>{{ trans('file.The field labels marked with * are required input fields') }}.</small>
+                    </p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="rfq_id">{{ trans('file.RFQ') }}:</label><span class="required">
+                                    *</span>
+                                <select name="rfq_id" id="rfq_id" class="form-control selectpicker" required>
+                                    <option value="">{{ trans('file.Select RFQ') }}</option>
+                                    @foreach ($rfQs as $item)
+                                        <option value="{{ $item->id }}">{{ $item->rfq_no }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="supplier_id">{{ trans('file.Supplier') }}</label><span class="required">
+                                    *</span>
+                                <select name="supplier_id" id="supplier_id" class="form-control selectpicker" required>
+                                    <option value="">{{ trans('file.Select Supplier') }}</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     </section>
 @endsection
 @push('scripts')
