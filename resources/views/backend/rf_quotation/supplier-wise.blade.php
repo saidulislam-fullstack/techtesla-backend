bc079de35998a8259b3e6158027c434e9783ddf5
Syed Shazeedul Islam <syedshazeedul@gmail.com>
2025-02-15 22:13:58 +0600

    supplier wise blade show done

--- resources/views/backend/rf_quotation/supplier-wise.blade.php
+++ resources/views/backend/rf_quotation/supplier-wise.blade.php
@@ -15,40 +15,33 @@
                             <div class="form-group">
                                 <label for="rfq_id">{{ trans('file.RFQ') }}:</label><span class="required">
                                     *</span>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                                <select name="rfq_id" id="rfq_id" class="form-control selectpicker"
-                                    onchange="selectRFQ(this)" required>
-=======
-                                <select name="rfq_id" id="rfq_id" class="form-control selectpicker" required>
->>>>>>> 02855dc (refactor)
-=======
-                                <select name="rfq_id" id="rfq_id" class="form-control selectpicker"
-                                    onchange="selectRFQ(this)" required>
->>>>>>> 1e602b9 (refactor)
-                                    <option value="">{{ trans('file.Select RFQ') }}</option>
-                                    @foreach ($rfQs as $item)
-                                        <option value="{{ $item->id }}">{{ $item->rfq_no }}</option>
-                                    @endforeach
-                                </select>
+                                <<<<<<< HEAD <<<<<<< HEAD <select name="rfq_id" id="rfq_id"
+                                    class="form-control selectpicker" onchange="selectRFQ(this)" required>
+                                    =======
+                                    <select name="rfq_id" id="rfq_id" class="form-control selectpicker" required>
+                                        >>>>>>> 02855dc (refactor)
+                                        =======
+                                        <select name="rfq_id" id="rfq_id" class="form-control selectpicker"
+                                            onchange="selectRFQ(this)" required>
+                                            >>>>>>> 1e602b9 (refactor)
+                                            <option value="">{{ trans('file.Select RFQ') }}</option>
+                                            @foreach ($rfQs as $item)
+                                                <option value="{{ $item->id }}">{{ $item->rfq_no }}</option>
+                                            @endforeach
+                                        </select>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="supplier_id">{{ trans('file.Supplier') }}</label><span class="required">
                                     *</span>
-<<<<<<< HEAD
                                 <select name="supplier_id" id="supplier_id" class="form-control selectpicker"
                                     onchange="selectSuppler(this)" required>
-=======
-                                <select name="supplier_id" id="supplier_id" class="form-control selectpicker" required>
->>>>>>> 02855dc (refactor)
                                     <option value="">{{ trans('file.Select Supplier') }}</option>
                                 </select>
                             </div>
                         </div>
                     </div>
-<<<<<<< HEAD
                     <div class="row">
                         <div class="table-responsive">
                             <table class="table table-bordered table-striped" id="supplier-wise-table">
@@ -92,8 +85,6 @@
                             </table>
                         </div>
                     </div>
-=======
->>>>>>> 02855dc (refactor)
                 </div>
             </div>
     </section>
@@ -108,7 +99,6 @@
 
         function selectRFQ(e) {
             let rfq_id = $(e).val();
-<<<<<<< HEAD
             let supplier_id = $('#supplier_id');
             supplier_id.empty();
             let rFQ = rFQs.find(rfq => rfq.id == rfq_id);
@@ -207,26 +197,5 @@
             $('#total_quantity').text(total_quantity);
             $('#grand_total').text(total.toFixed(2));
         }
-=======
-            console.log(rfq_id);
-            let supplier_id = $('#supplier_id');
-            supplier_id.empty();
-            let rFQ = rFQs.find(rfq => rfq.id == rfq_id);
-            console.log(rFQ);
-            if (rFQ) {
-                let suppliers = rFQ.price_collection
-                    .filter(item => item.is_selected == 1)
-                    .map(item => item.supplier)
-                    .filter((value, index, self) => self.findIndex(s => s.id === value.id) === index);
-
-                console.log(suppliers);
-                if (suppliers.length > 0) {
-                    suppliers.forEach(supplier => {
-                        supplier_id.append(`<option value="${supplier.id}">${supplier.name}</option>`);
-                    });
-                }
-            }
-        }
->>>>>>> 1e602b9 (refactor)
     </script>
 @endpush
