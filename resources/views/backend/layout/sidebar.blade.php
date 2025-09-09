590e4a8e42e20b30f75d40c9b18bf80baae9d663
Saidul Islam <saidulislam197120@gmail.com>
2025-09-09 22:15:40 +0600

    Renamed some menus

--- resources/views/backend/layout/sidebar.blade.php
+++ resources/views/backend/layout/sidebar.blade.php
@@ -193,14 +193,14 @@
                     </li>
                 @endif
                 <li id="rf-quotation-list-menu">
-                    <a href="{{ route('rf-quotation.index') }}">{{ trans('file.RFQuotation List') }}</a>
+                    <a href="{{ route('rf-quotation.index') }}">RFQ List</a>
                 </li>
                 @php
                     $add_permission_active = $role_has_permissions_list->where('name', 'rf-quotes-add')->first();
                 @endphp
                 @if ($add_permission_active)
                     <li id="rf-quotation-create-menu">
-                        <a href="{{ route('rf-quotation.create') }}">{{ trans('file.Add RFQuotation') }}</a>
+                        <a href="{{ route('rf-quotation.create') }}">Add RFQ</a>
                     </li>
                 @endif
                 @php
