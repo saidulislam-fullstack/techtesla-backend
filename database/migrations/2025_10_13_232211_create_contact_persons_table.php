adf7d22652923eced898e316e17f94cc41d22ce5
Saidul Islam <saidulislam197120@gmail.com>
2025-10-13 23:54:16 +0600

    modified the contact person resources

--- database/migrations/2025_10_13_232211_create_contact_persons_table.php
+++ database/migrations/2025_10_13_232211_create_contact_persons_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contact_people', function (Blueprint $table) {
+        Schema::create('contact_persons', function (Blueprint $table) {
             $table->id();
             $table->string('contactable_type');
             $table->unsignedBigInteger('contactable_id');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contact_people');
+        Schema::dropIfExists('contact_persons');
     }
 };
