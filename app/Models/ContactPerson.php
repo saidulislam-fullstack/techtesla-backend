2e62e5a7ad84da42fe849f533e77c31f05af2242
Saidul Islam <saidulislam197120@gmail.com>
2025-10-13 23:30:07 +0600

    Adding contact person resources

---
+++ app/Models/ContactPerson.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactPerson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'contactable_type',
+        'contactable_id',
+        'name',
+        'email',
+        'phone',
+        'designation',
+        'visiting_card_front',
+        'visiting_card_back',
+    ];
+
+    public function contactable()
+    {
+        return $this->morphTo();
+    }
+}
---
+++ database/migrations/2025_10_13_232211_create_contact_people_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_people', function (Blueprint $table) {
+            $table->id();
+            $table->string('contactable_type');
+            $table->unsignedBigInteger('contactable_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('designation')->nullable();
+            $table->string('visiting_card_front')->nullable();
+            $table->string('visiting_card_back')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_people');
+    }
+};
